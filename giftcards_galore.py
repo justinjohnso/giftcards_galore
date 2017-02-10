eec9005a93682ef7438096f1a375389d4326b8ca
Justin Johnson <justinaugustejohnson@gmail.com>
2017-02-10 14:36:01 -0500

    init commit

---
+++ giftcards_galore.py
@@ -0,0 +1,56 @@
+"""Autobuy $.50 Amazon Giftcards"""
+
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
+from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
+
+# User-defined variables
+AMAZON_USERNAME = os.getenv('AMAZON_USERNAME')
+    # Your Amazon username (email)
+AMAZON_PASSWORD = os.getenv('AMAZON_PASSWORD')
+    # Your Amazon password
+CARDS = [1, 3, 2, 4]
+    # 0-indexed array of your Amazon payment methods
+    # Refer to https://www.amazon.com/gp/wallet
+CARD_NUMBERS = [
+    os.getenv('TD_DEBIT'),
+    os.getenv('CSR'),
+    os.getenv('CFU'),
+    os.getenv('CF2')]
+    # Your credit card numbers, corresponding to the index of each card in the CARDS array
+ITERATIONS = [3, 1, 12, 8]
+    # Iterations array, corresponds to the number of purchases for each card
+
+def giftcard_buyer():
+    "Function to buy the giftcards"
+    driver = webdriver.Chrome('/Users/Justin/code/packages/chromedriver')
+    wait = WebDriverWait(driver, 10)
+    driver.get('http://www.amazon.com')
+    driver.find_element_by_link_text('Sign in').click()
+    driver.find_element_by_id('ap_email').send_keys(AMAZON_USERNAME)
+    driver.find_element_by_id('ap_password').send_keys(AMAZON_PASSWORD)
+    driver.find_element_by_id('signInSubmit').click()
+
+    i = 0
+    for card in CARDS:
+        print "card: %r" %(card)
+        for iteration in range(ITERATIONS[i]):
+            print "iteration: %r" %(iteration + 1)
+            driver.get('https://www.amazon.com/asv/reload/')
+            wait.until(EC.title_is('Reload Your Balance'))
+            driver.find_element_by_id('asv-manual-reload-amount').send_keys('0.5')
+            time.sleep(1)
+            driver.find_elements_by_class_name('pmts-instrument-box')[card].click()
+            if iteration == 0:
+                driver.find_element_by_name('addCardNumber').send_keys(CARD_NUMBERS[card - 1])
+                driver.find_element_by_xpath("//button[contains(.,'Confirm Card')]").click()
+                time.sleep(1)
+            # driver.find_element_by_id('form-submit-button').click()
+            # time.sleep(2)
+        i += 1
+
+giftcard_buyer()
---
+++ readme.md
@@ -0,0 +1,1 @@
+Python script to auto-buy n Amazon giftcards per payment method
